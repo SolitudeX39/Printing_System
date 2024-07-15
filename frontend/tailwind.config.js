4a1025db4dd57ab01cc1e08ae2855ff6b2f499aa
Zotero Disuke <zoterodisuke@gmail.com>
2024-07-15 11:23:24 +0700

    sss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js,jsx}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                custom: [
-                    "IBM Plex Sans Thai", "sans-serif"
-                ],
-                custom2: ["Kodchasan", "sans-serif"]
-            }
-        }
-    },
     plugins: []
 }
